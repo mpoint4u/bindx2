951cd07fdefe27927e0338cc48c407229f596161
Marcus Pointner <marcus.pointner@gmail.com>
2014-12-07 11:37:58 +0200

    making this branch unique
    just to not get confused with the different versions

--- samples/userInfo/src/Main.hx
+++ samples/userInfo/src/Main.hx
@@ -2,15 +2,15 @@ package ;
 
 class Main {
     static function main() {
-        var user = new UserModel("deep", 100);
+        var user = new UserModel("maxx", 199);
         var view = new UserView();
         view.user = user; 
         //> TextField::text changed to: Hello deep. You have 100 coins
         //> user health changed from null to 1
         
-        user.coins = 200; //> TextField::text changed to: Hello deep. You have 200 coins
+        user.coins = 299; //> TextField::text changed to: Hello deep. You have 200 coins
         
-        //user.health = 0.5; //> user health changed from 1 to 0.5
+        user.health = 0.5; //> user health changed from 1 to 0.5
         
         view.user = null;
         
@@ -20,7 +20,7 @@ class Main {
         //> TextField::text changed to: Hello profelis. You have 200 coins
         //> user health changed from null to 1
         
-        view.user.coins = 300; //> TextField::text changed to: Hello profelis. You have 300 coins
+        view.user.coins = 399; //> TextField::text changed to: Hello profelis. You have 300 coins
         
         user.health = 0.75; //> user health changed from 1 to 0.75
     }
